4a5fe975abc59b4ee0bb44fe5eeeeed2d1208c0f
Cameron Micka <cameron.micka@microsoft.com>
2024-04-05 14:45:56 -0700

    Simple performance scene.

--- VisualProfilerUnityProject/Assets/Scripts/SampleControls.cs
+++ VisualProfilerUnityProject/Assets/Scripts/SampleControls.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Microsoft.MixedReality.Profiling.Sample
@@ -10,10 +12,13 @@ namespace Microsoft.MixedReality.Profiling.Sample
         public GameObject Profiler3D;
         public GameObject Profiler2D;
 
+        public GameObject Ball;
+        public Transform BallSpawn;
+
         private int selectionIndex = 0;
         private string[] selectionStrings = { "Profiler 3D", "Profiler 2D" };
 
-        void OnGUI()
+        private void OnGUI()
         {
             selectionIndex = GUI.SelectionGrid(new Rect(10, 10, 160, 20), selectionIndex, selectionStrings, 2);
 
@@ -32,6 +37,32 @@ namespace Microsoft.MixedReality.Profiling.Sample
                         Profiler2D.SetActive(true);
                     }
                     break;
+            }
+
+            if (GUI.Button(new Rect(10, 40, 80, 20), "Reset Balls"))
+            {
+                if (BallSpawn != null)
+                {
+                    foreach (Transform child in BallSpawn.transform)
+                    {
+                        Destroy(child.gameObject);
+                    }
+                }
+            }
+        }
+
+        private void Start()
+        {
+            StartCoroutine(SpawnBalls());
+        }
+
+        private IEnumerator SpawnBalls()
+        {
+            while (Ball != null && BallSpawn != null) 
+            {
+                Instantiate(Ball, BallSpawn).GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere;
+
+                yield return new WaitForSeconds(0.1f);
             }
         }
     }
